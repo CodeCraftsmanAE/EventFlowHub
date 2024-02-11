3bed7744920f722b2356bfc6a8a758f9a7ea1585
Ahmed Elatrouz <ahmed.elatrouz@orange.com>
2024-02-11 20:32:04 +0100

    add first microservice DatasourceToKafkaService

---
+++ datasource-to-kafka-service/src/main/java/com/EventFlowHub/datasource/to/kafka/service/DatasourceToKafkaServiceApplication.java
@@ -0,0 +1,12 @@
+package com.EventFlowHub.datasource.to.kafka.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasourceToKafkaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasourceToKafkaServiceApplication.class, args);
+	}
+}
---
+++ datasource-to-kafka-service/src/test/java/com/EventFlowHub/datasource/to/kafka/service/DatasourceToKafkaServiceApplicationTest.java
@@ -0,0 +1,16 @@
+package com.EventFlowHub.datasource.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DatasourceToKafkaServiceApplicationTest {
+
+	@Test
+	public void contextLoad() {
+
+	}
+
+}
